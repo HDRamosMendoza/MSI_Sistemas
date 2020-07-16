3adc75160ae211eac0c6af31644abe7f1dd7968b
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2020-07-16 14:29:55 -0500

    Prueba de concepto LEAFLET

--- Seguridad/js/leaflet.js
+++ Seguridad/js/leaflet.js
@@ -16,10 +16,18 @@
 		streets  = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
 
 	var map = L.map('map', {
-		center: [39.73, -104.99],
-		zoom: 10,
+		center: [-12.099167, -77.034722],
+    	zoom: 14,
 		layers: [grayscale, cities]
 	});
+
+	map.createPane('labels');
+
+	// This pane is above markers but below popups
+	map.getPane('labels').style.zIndex = 650;
+
+	// Layers in this pane are non-interactive and do not obscure mouse/touch events
+	map.getPane('labels').style.pointerEvents = 'none';
 
 	var baseLayers = {
 		"Grayscale": grayscale,
@@ -31,3 +39,101 @@
 	};
 
 	L.control.layers(baseLayers, overlays).addTo(map);
+
+/************************************************** BORRADOR */
+
+let schools;
+sql_text = '1=1';
+/* CBO - Change */
+fnLoadSector = function(idElement,urlWFS) {
+    try { /* DOM - Checkbox (input) */
+        const nodeCheckbox = document.getElementById(idElement);
+        $.ajax( urlWFS, {
+	  		type: 'GET',
+	  		data: {
+	      		service: 'WFS',
+	      		request: 'GetFeature',
+	      		typeName: 'msigeoportal:sec_catastro',
+	      		CQL_FILTER: sql_text,
+	      		srsname: 'EPSG:4326',
+	      		outputFormat: 'application/json'
+	    	}
+	  	}).done(function(item) { 	
+		  	let resultItems = [];
+		  	features = item.features;
+		  	itemLength = features.length;
+            for(let i = 0; i < itemLength; i++) {
+                resultItems.push('\
+                	<option data-heber="asa"\
+                		value="' + features[i].properties.id_sector +'" >'
+                		+ features[i].properties.sector + 
+                	'</option>'
+                );
+            }
+            resultItems = resultItems.sort();
+            localStorage.setItem('sector', JSON.stringify(features));
+            resultItems.unshift("<option data-heber='asa' value=\"\"> - Seleccione - </option>");
+		  	document.getElementById(idElement).innerHTML = resultItems.join("");
+
+		  	
+		});
+    } catch (error) { console.error(error); }
+};
+
+/* CBO - Sector */
+fnLoadSector('SectorID','http://geo.munisanisidro.gob.pe:8080/geoserver/msigeoportal/wfs');
+let multipolygonGeojson;
+fnChangeSector = function(idElement) {
+    try { /* DOM - Checkbox (input) */
+        const nodeCheckbox = document.getElementById(idElement);
+        nodeCheckbox.addEventListener('change', function(event) {
+            event.preventDefault();
+            let idValue = this.value.trim();
+            //map.panTo(new L.LatLng(40.737, -73.923));
+            //map.setView(new L.LatLng(40.737, -73.923), 8);
+            schools.setParams({cql_filter:'id_sector=' + idValue });
+            let cboSector = JSON.parse(localStorage.getItem('sector'));
+		  	for (var i = 0; i < cboSector.length; i++) {
+
+		  		if(cboSector[i].properties.id_sector == idValue) {
+		  			/* Borrar la capa */
+					map.eachLayer( function(layer) {
+          				if ( layer.myTag &&  layer.myTag === "myGeoJSON") {
+            				map.removeLayer(layer)
+              			}
+            		});
+            		/* Adicional al mapa y un popup */
+		  			multipolygonGeojson = L.geoJson(cboSector[i]).addTo(map);
+		  			multipolygonGeojson.eachLayer(function (layer) {
+		  				/* Configuracion del POPUP */
+						layer.bindPopup("<strong>SECTOR: " + layer.feature.properties.sector + "</strong>").openPopup();
+						layer.myTag = "myGeoJSON";
+					});
+
+		  			map.fitBounds(multipolygonGeojson.getBounds());
+		  		}
+		  	}
+        });
+    } catch (error) { console.error(error); }
+};
+
+/* CBO - Sector */
+fnChangeSector('SectorID');
+
+/*Configurando*/
+function fnTileLayer (){
+     //defeine schools as a global variable, without var keyword so it can be checked at set_sql function
+     schools = L.tileLayer.wms('http://geo.munisanisidro.gob.pe:8080/geoserver/msigeoportal/wms', {
+            layers: 'msigeoportal:sec_catastro',
+            format: 'image/png',
+            transparent: true,
+            maxZoom: 30,
+            opacity: 0.5,
+            cql_filter: '1=2',
+            pane: 'labels'
+        }).addTo(map);
+
+     //console.log(schools);
+}
+
+fnTileLayer();
